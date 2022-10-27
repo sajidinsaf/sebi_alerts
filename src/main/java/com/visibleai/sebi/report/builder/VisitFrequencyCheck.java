3cb369bdec7ebe45c8704725b3b0d9641ffb73cf
Aarish-OIS <aarishois@Aarishs-MacBook-Pro.local>
2022-10-27 23:14:10 +0530

    Tested the visit frequency check.

---
+++ src/main/java/com/visibleai/sebi/report/builder/VisitFrequencyCheck.java
@@ -0,0 +1,32 @@
+package com.visibleai.sebi.report.builder;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import com.visibleai.sebi.report.builder.model.FrequentVisitorDetail;
+
+public class VisitFrequencyCheck {
+    private int numberOfDays;
+
+    public VisitFrequencyCheck(int numberOfDaysInThisPeriod) {
+        numberOfDays = numberOfDaysInThisPeriod;
+    }
+
+    public boolean check(FrequentVisitorDetail frequentVisitorDetail) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MMM yyyy hh:mma");
+        String timeIn = frequentVisitorDetail.getTimeIn();
+        Date entryTime = simpleDateFormat.parse(timeIn);
+        Date todaysDate = new Date();
+        Calendar numberOfDaysAgo = Calendar.getInstance();
+        numberOfDaysAgo.setTime(todaysDate);
+        numberOfDaysAgo.add(Calendar.DATE, -numberOfDays);
+        Date numberOfDaysAgoDate = numberOfDaysAgo.getTime();
+        boolean isEntryTimeBeforeToday = todaysDate.after(entryTime);
+        boolean isEntryTimeAfterStartDate = numberOfDaysAgoDate.before(entryTime);
+        return isEntryTimeBeforeToday && isEntryTimeAfterStartDate; // if statement
+
+    }
+
+}
---
+++ src/test/java/com/visibleai/sebi/report/builder/VisitFrequencyCheckTest.java
@@ -0,0 +1,89 @@
+package com.visibleai.sebi.report.builder;
+
+import static org.junit.Assert.assertEquals;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Test;
+
+import com.visibleai.sebi.report.builder.model.FrequentVisitorDetail;
+
+public class VisitFrequencyCheckTest {
+
+    @Test
+    public void testSevenDaysAgoCheck() throws ParseException {
+
+        int period = 7;
+        VisitFrequencyCheck visitFrequencyCheck = new VisitFrequencyCheck(period);
+
+        // dd MMM yyyy hh:mma
+        FrequentVisitorDetail visitBetweenPeriodStartAndEndDate = new FrequentVisitorDetail();
+
+        Date todaysDate = new Date();
+        Calendar numberOfDaysAgo = Calendar.getInstance();
+        numberOfDaysAgo.setTime(todaysDate);
+        numberOfDaysAgo.add(Calendar.DATE, -period + 3);
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MMM yyyy hh:mma");
+
+        String dateOfVisitBetweenPeriodStartAndEndDate = simpleDateFormat.format(numberOfDaysAgo.getTime());
+
+        numberOfDaysAgo.setTime(todaysDate);
+        numberOfDaysAgo.add(Calendar.DATE, -period - 3);
+
+        String dateOfVisitBeforePeriodStartDate = simpleDateFormat.format(numberOfDaysAgo.getTime());
+
+        numberOfDaysAgo.setTime(todaysDate);
+        numberOfDaysAgo.add(Calendar.DATE, 10);
+
+        String dateOfVisitAfterPeriodEndDate = simpleDateFormat.format(numberOfDaysAgo.getTime());
+
+        visitBetweenPeriodStartAndEndDate.setTimeIn(dateOfVisitBetweenPeriodStartAndEndDate);
+
+        FrequentVisitorDetail visitBeforePeriodStartDate = new FrequentVisitorDetail();
+        visitBeforePeriodStartDate.setTimeIn(dateOfVisitBeforePeriodStartDate);
+
+        FrequentVisitorDetail visitAfterPeriodEndDate = new FrequentVisitorDetail();
+        visitAfterPeriodEndDate.setTimeIn(dateOfVisitAfterPeriodEndDate);
+
+        boolean resultOfBetweenPeriod = visitFrequencyCheck.check(visitBetweenPeriodStartAndEndDate);
+        boolean resultOfBeforePeriod = visitFrequencyCheck.check(visitBeforePeriodStartDate);
+        boolean resultOfAfterPeriod = visitFrequencyCheck.check(visitAfterPeriodEndDate);
+
+        assertEquals(true, resultOfBetweenPeriod);
+        assertEquals(false, resultOfBeforePeriod);
+        assertEquals(false, resultOfAfterPeriod);
+    }
+
+    public void testEntryDays() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        String todaysDay = "27/10/2022";
+        String entryDay1 = "20/10/2022";
+        String entryDay2 = "23/10/2022";
+        String entryDay3 = "25/10/2022";
+        String startDate = "20/10/2022";
+
+        // convert String to LocalDate
+        LocalDate reportDay = LocalDate.parse(todaysDay, formatter);
+        LocalDate dateOfEntry1 = LocalDate.parse(entryDay1, formatter);
+        LocalDate dateOfEntry2 = LocalDate.parse(entryDay2, formatter);
+        LocalDate dateOfEntry3 = LocalDate.parse(entryDay3, formatter);
+        LocalDate startDay = LocalDate.parse(startDate, formatter);
+
+        assertEquals(reportDay, reportDay.isAfter(dateOfEntry1));
+        assertEquals(startDay, reportDay.isBefore(dateOfEntry1));
+
+        assertEquals(reportDay, reportDay.isAfter(dateOfEntry2));
+        assertEquals(startDay, reportDay.isBefore(dateOfEntry2));
+
+        assertEquals(reportDay, reportDay.isAfter(dateOfEntry3));
+        assertEquals(startDay, reportDay.isBefore(dateOfEntry3));
+    }
+
+}
