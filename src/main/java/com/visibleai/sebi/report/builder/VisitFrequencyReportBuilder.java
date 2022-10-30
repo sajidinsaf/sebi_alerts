8cb45f5897d99d6e71b31fe5364304641ef9550f
Aarish-OIS <aarishois@Aarishs-MacBook-Pro.local>
2022-10-30 18:30:51 +0530

    Tested and edited the VisitFrequencyReportBuilder

--- src/main/java/com/visibleai/sebi/report/builder/VisitFrequencyReportBuilder.java
+++ src/main/java/com/visibleai/sebi/report/builder/VisitFrequencyReportBuilder.java
@@ -2,73 +2,96 @@ package com.visibleai.sebi.report.builder;
 
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.visibleai.sebi.model.VisitorEntry;
 import com.visibleai.sebi.report.Report;
+import com.visibleai.sebi.report.SebiAlertsReport;
+import com.visibleai.sebi.report.TableReportData;
 import com.visibleai.sebi.report.builder.model.FrequentVisitorDetail;
 import com.visibleai.sebi.validation.VisitFrequencyCheck;
 
 public class VisitFrequencyReportBuilder implements ReportBuilder {
 
-    private VisitFrequencyCheck sevenDayCheck;
-    private VisitFrequencyCheck fourteenDayCheck;
-    private VisitFrequencyCheck thirtyDayCheck;
-    private Map<String, List<FrequentVisitorDetail>> sevenDayMap;
-    private Map<String, List<FrequentVisitorDetail>> fourteenDayMap;
-    private Map<String, List<FrequentVisitorDetail>> thirtyDayMap;
+    private SebiAlertsReport visitFrequencyReport = null;
+    private VisitFrequencyCheck visitFrequencyCheck;
+    private Map<String, List<FrequentVisitorDetail>> visitFrequencyCheckMap;
+    private int frequencyViolationNumber;
 
-    public VisitFrequencyReportBuilder() {
-        sevenDayCheck = new VisitFrequencyCheck(7);
-        fourteenDayCheck = new VisitFrequencyCheck(14);
-        thirtyDayCheck = new VisitFrequencyCheck(30);
-        sevenDayMap = new HashMap<String, List<FrequentVisitorDetail>>();
-        fourteenDayMap = new HashMap<String, List<FrequentVisitorDetail>>();
-        thirtyDayMap = new HashMap<String, List<FrequentVisitorDetail>>();
+    public VisitFrequencyReportBuilder(int numberOfDays, int frequencyViolationNumber) {
+        visitFrequencyCheck = new VisitFrequencyCheck(numberOfDays);
+        visitFrequencyCheckMap = new HashMap<String, List<FrequentVisitorDetail>>();
+        visitFrequencyReport = new SebiAlertsReport();
+        visitFrequencyReport.setTitle(numberOfDays + " Day Check Report");
+        visitFrequencyReport.setDate(new Date());
+        this.frequencyViolationNumber = frequencyViolationNumber;
+
     }
 
     public void build(VisitorEntry visitorEntry) {
         FrequentVisitorDetail frequentVisitorDetail = new FrequentVisitorDetail(visitorEntry);
 
         try {
-            boolean sevenDayCheckResult = sevenDayCheck.check(frequentVisitorDetail);
-            boolean fourteenDayCheckResult = fourteenDayCheck.check(frequentVisitorDetail);
-            boolean thirtyDayCheckResult = thirtyDayCheck.check(frequentVisitorDetail);
+            boolean visitFrequencyDayCheckResult = visitFrequencyCheck.check(frequentVisitorDetail);
 
             String phoneNumber = frequentVisitorDetail.getVisitorNumber();
 
-            if (sevenDayCheckResult) {
-                List<FrequentVisitorDetail> frequentVisitorDetailList = sevenDayMap.getOrDefault(phoneNumber,
+            if (visitFrequencyDayCheckResult) {
+                List<FrequentVisitorDetail> frequentVisitorDetailList = visitFrequencyCheckMap.getOrDefault(phoneNumber,
                         new ArrayList<FrequentVisitorDetail>());
                 frequentVisitorDetailList.add(frequentVisitorDetail);
-                sevenDayMap.put(phoneNumber, frequentVisitorDetailList);
-            }
-
-            if (fourteenDayCheckResult) {
-                List<FrequentVisitorDetail> frequentVisitorDetailList = fourteenDayMap.getOrDefault(phoneNumber,
-                        new ArrayList<FrequentVisitorDetail>());
-                frequentVisitorDetailList.add(frequentVisitorDetail);
-                fourteenDayMap.put(phoneNumber, frequentVisitorDetailList);
-            }
-            if (thirtyDayCheckResult) {
-                List<FrequentVisitorDetail> frequentVisitorDetailList = thirtyDayMap.getOrDefault(phoneNumber,
-                        new ArrayList<FrequentVisitorDetail>());
-                frequentVisitorDetailList.add(frequentVisitorDetail);
-                thirtyDayMap.put(phoneNumber, frequentVisitorDetailList);
+                visitFrequencyCheckMap.put(phoneNumber, frequentVisitorDetailList);
             }
 
         } catch (ParseException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
     }
 
     public Report getReport() {
-        // TODO Auto-generated method stub
-        return null;
+
+        TableReportData visitFrequencyReportData = new TableReportData();
+
+        List<String> header = Arrays.asList("Phone Number", "Name", "Meeting with", "Date", "Visitors Company",
+                "Type Of Visitor", "Comments");
+        visitFrequencyReportData.setHeader(header);
+
+        for (String phoneNumber : visitFrequencyCheckMap.keySet()) {
+            List<FrequentVisitorDetail> frequentVisitorDetailList = visitFrequencyCheckMap.get(phoneNumber);
+            if (frequentVisitorDetailList.size() >= frequencyViolationNumber) {
+                // "Phone Number", "Name", "Meeting with", "Date", "Visitors Company", "Type Of
+                // Visitor", "Comments"
+                // "Phone Number", "Name1|Name2|Name3 ", "Meeting with1|Meeting With2|Meeting
+                // With3", "Date1|Date2|Date3", "Visitors Company1|Visitors Company2|Visitors
+                // Company3", "Type Of Visitor1|Type Of Visitor2|Type Of Visitor3", "Comments"
+
+                String names = "";
+                String meetingsWith = "";
+                String visitDates = "";
+                String visitorCompanies = "";
+                String typeOfVisitor = "";
+                String comments = "";
+                for (FrequentVisitorDetail frequentVisitorDetail : frequentVisitorDetailList) {
+                    names = names + frequentVisitorDetail.getVisitorName() + "|";
+                    meetingsWith = meetingsWith + frequentVisitorDetail.getToMeet() + "|";
+                    visitDates = visitDates + frequentVisitorDetail.getTimeIn() + "|";
+                    visitorCompanies = visitorCompanies + frequentVisitorDetail.getVisitorCompany() + "|";
+                    typeOfVisitor = typeOfVisitor + frequentVisitorDetail.getAccessCardId() + "|";
+                }
+
+                List<String> row = Arrays.asList(phoneNumber, names, meetingsWith, visitDates, visitorCompanies,
+                        typeOfVisitor, comments);
+                visitFrequencyReportData.addRow(row);
+            }
+
+        }
+        visitFrequencyReport.setReportData(visitFrequencyReportData);
+        return visitFrequencyReport;
     }
 
 }
--- src/test/java/com/visibleai/sebi/validation/VisitFrequencyCheckTest.java
+++ src/test/java/com/visibleai/sebi/validation/VisitFrequencyCheckTest.java
@@ -1,11 +1,9 @@
-package com.visibleai.sebi.report.builder;
+package com.visibleai.sebi.validation;
 
 import static org.junit.Assert.assertEquals;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -13,7 +11,6 @@ import org.junit.Test;
 
 import com.visibleai.sebi.model.Constants;
 import com.visibleai.sebi.report.builder.model.FrequentVisitorDetail;
-import com.visibleai.sebi.validation.VisitFrequencyCheck;
 
 public class VisitFrequencyCheckTest {
 
@@ -59,32 +56,7 @@ public class VisitFrequencyCheckTest {
         assertEquals(true, resultOfBetweenPeriod);
         assertEquals(false, resultOfBeforePeriod);
         assertEquals(false, resultOfAfterPeriod);
-    }
 
-    public void testEntryDays() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-        String todaysDay = "27/10/2022";
-        String entryDay1 = "20/10/2022";
-        String entryDay2 = "23/10/2022";
-        String entryDay3 = "25/10/2022";
-        String startDate = "20/10/2022";
-
-        // convert String to LocalDate
-        LocalDate reportDay = LocalDate.parse(todaysDay, formatter);
-        LocalDate dateOfEntry1 = LocalDate.parse(entryDay1, formatter);
-        LocalDate dateOfEntry2 = LocalDate.parse(entryDay2, formatter);
-        LocalDate dateOfEntry3 = LocalDate.parse(entryDay3, formatter);
-        LocalDate startDay = LocalDate.parse(startDate, formatter);
-
-        assertEquals(reportDay, reportDay.isAfter(dateOfEntry1));
-        assertEquals(startDay, reportDay.isBefore(dateOfEntry1));
-
-        assertEquals(reportDay, reportDay.isAfter(dateOfEntry2));
-        assertEquals(startDay, reportDay.isBefore(dateOfEntry2));
-
-        assertEquals(reportDay, reportDay.isAfter(dateOfEntry3));
-        assertEquals(startDay, reportDay.isBefore(dateOfEntry3));
     }
 
 }
